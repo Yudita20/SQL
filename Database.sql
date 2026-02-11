373e4ccd422d462c94888843b8ec31d12fac1914
Yudita_Khanodia <yudikhanodia2005@gmail.com>
2026-02-11 17:10:57 +0530

    Creation and Deletion of Database.

---
+++ Database.sql
@@ -0,0 +1,13 @@
+-- Queries for creation and deletion of 
+-- a database 
+
+-- SQL IS A CASE-INSENSITIVE LANGAUAGE
+
+-- Creating a database
+CREATE DATABASE College;
+
+-- Using a database
+USE College;
+
+-- Droping a database
+-- DROP DATABASE College;
