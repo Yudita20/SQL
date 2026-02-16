6c79b7f9bc1e09b4e839f9661502341e2fd2e5bf
Yudita_Khanodia <yudikhanodia2005@gmail.com>
2026-02-16 21:25:20 +0530

    In and Not In

---
+++ IN.sql
@@ -0,0 +1,9 @@
+SELECT * FROM customers;
+
+SELECT * FROM customers
+WHERE Country IN('France' , 'India' , 'London');
+
+SELECT * FROM customers
+WHERE Country NOT IN('France' , 'India' , 'London');
+
+
